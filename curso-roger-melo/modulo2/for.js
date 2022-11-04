09e2303c1e771332e135b6ffd7b40347a547e399
Kened Silva Felix <ksfelix1@stefanini.com>
2022-11-04 08:21:04 -0300

    Treinamentos loops

---
+++ curso-roger-melo/modulo2/for.js
@@ -0,0 +1,11 @@
+//for
+
+for(let i =0; i < 5; i++){
+  console.log(`Dentro do loop'${i}`);
+}
+
+const names = ['linus', 'Ada', 'Bill']
+
+for(let i =0; i < names.length; i++){
+  console.log(names[i]);
+}
---
+++ curso-roger-melo/modulo2/while.js
@@ -0,0 +1,7 @@
+let i = 0;
+const names = ['Jobs', 'Hooper','Turing'];
+
+while(i < names.length){
+  console.log(names[i])
+ i++;
+}
